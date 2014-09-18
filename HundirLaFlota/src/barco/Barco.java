e7ae9f3084c71ecd80b5ed064c47b030ee0a1758
DavidCatalan <freedavid94@gmail.com>
2014-09-18 13:58:04 +0200

    Clase barco añadida.

---
+++ HundirLaFlota/src/barco/Barco.java
@@ -0,0 +1,8 @@
+package barco;
+
+public class Barco {
+	int filaInicial, columnaInicial; // coordenadas iniciales del barco
+	char orientacion; // 'H': horizontal; 'V': vertical;
+	int tamanyo; // numero de casillas que ocupa
+	int tocadas; // numero de casillas tocadas
+}
