6c29f6c5b2db116299dce7aa1075b31e36ca90e0
ferran294 <ferran294@gmail.com>
2014-09-22 12:35:34 +0200

    Nueva clase partida

---
+++ HundirLaFlota/src/barco/Partida.java
@@ -0,0 +1,14 @@
+package barco;
+
+import java.util.Vector;
+
+public class Partida {
+	private int[][] mar;
+	private int numFilas, numColumnas;
+	private Vector<Barco> barcos = null;
+	private int numBarcos; // barcos incluidos en la partida
+	private int quedan; // barcos sin hundir
+	private int disparos; // disparos efectuados
+	
+	
+}
