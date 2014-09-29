ee8f03f07bd877d3c78450bc36f6778641887afb
DavidCatalan <freedavid94@gmail.com>
2014-09-29 12:21:04 +0200

    MenuListener terminado

--- HundirLaFlota/src/tablero/Juego.java
+++ HundirLaFlota/src/tablero/Juego.java
@@ -79,10 +79,12 @@ public class Juego {
 		menu.add(salir);
 		
 		JMenuItem nuevaPartida = new JMenuItem("Nueva partida");
+		nuevaPartida.setActionCommand("nueva");
 		nuevaPartida.addActionListener(e);
 		menu.add(nuevaPartida);
 		
 		JMenuItem solucion = new JMenuItem("Mostrar solución");
+		solucion.setActionCommand("solucion");
 		solucion.addActionListener(e);
 		menu.add(solucion);
 		
@@ -145,7 +147,6 @@ public class Juego {
 	 * @param cadenaEstado	nuevo estado
 	 */
 	private void cambiaEstado(String cadenaEstado) {
-        // POR IMPLEMENTAR
 		estado.setText(cadenaEstado);
 	} // end cambiaEstado
 	
@@ -180,6 +181,16 @@ public class Juego {
 			switch(opcion){
 			case"salir":
 				System.exit(0);
+				break;
+			
+			case "nueva":
+				limpiaTablero();
+				break;
+			
+			case "solucion":
+				muestraSolucion();
+				break;
+				
 			}
 			
 			// POR IMPLEMENTAR	
