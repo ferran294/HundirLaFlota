2fdf360de4f853f4da3f623893cc593476f0c227
DavidCatalan <freedavid94@gmail.com>
2014-10-01 19:24:10 +0200

    Corregido warning unused import.

--- HundirLaFlota/src/tablero/Juego.java
+++ HundirLaFlota/src/tablero/Juego.java
@@ -2,7 +2,6 @@ package tablero;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
